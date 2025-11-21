dd9482ece687ace79642755ef63a15ed87187e3b
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-11-21 11:37:23 +0800

    sadgasgd

--- Assets/Scripts/Test/Scripst/Crafting/Item_Crafting.cs
+++ Assets/Scripts/Test/Scripst/Crafting/Item_Crafting.cs
@@ -6,6 +6,14 @@ public class Item_Crafting : ScriptableObject
     [Header("Item To Craft")]
     public Item_Create craft_item;
 
+    [Header("Material1")]
+    public Item_Create required_material1;
+    public int required_quantity1;
+
+    [Header("Material2")]
+    public Item_Create required_material2;
+    public int required_quantity2;
+
     [Header("Required Materials")]
     public MaterialRequirement[] craft_materials;
 }
